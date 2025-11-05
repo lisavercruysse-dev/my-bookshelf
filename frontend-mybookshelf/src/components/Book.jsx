687eb3186d3abe4353b400d77fac162e7d727c4b
Lisa Vercruysse <lisa.vercruyss@student.hogent.be>
2025-11-05 10:28:08 +0100

    Finish up book component

--- frontend-mybookshelf/src/components/Book.jsx
+++ frontend-mybookshelf/src/components/Book.jsx
@@ -1,23 +1,48 @@
+import { useState } from 'react';
+import altBook from '../assets/altBook.jpg';
+
 export default function Book (props) {
 
   const { isbn, title, genre, description, amountPages, author, img} = props;
+  const [isMoreInfo, setIsMoreInfo] = useState(false);
+  const [imgUrl, setImgUrl] = useState(img || altBook);
+  
+  const toggleInfo = () => {
+    setIsMoreInfo(!isMoreInfo);
+  };
 
   return (
-    <div>
-      <h4>
+    <div className="flex flex-col p-5 m-2 items-center w-xl">
+      <h4 className="text-center">
         {title}
       </h4>
-      <p>
+      <p className="italic">
         {author}
       </p>
-      <img src={img} alt="book cover"/>
+      <img
+        src={imgUrl}
+        alt="book cover"
+        onError={() => setImgUrl(altBook)}
+        className="w-48 rounded-lg border shadow-xl shadow-"
+      />
       <p>
         {amountPages}
-        <button>more</button>
-        ISBN: {isbn}
-        genre: {genre}
-        description: {description}
       </p>
+      <div className='items-center flex flex-col'>
+        <button className="border rounded-lg pl-3 pr-3 pt-1 pb-1 m-3" 
+          onClick={toggleInfo}>{isMoreInfo ? 'less' : 'more'}</button>
+        <div className={`${isMoreInfo ? 'block' : 'hidden'} flex flex-col border rounded-2xl p-5`}>
+          <p className='p-2'>
+            ISBN: {isbn}
+          </p>
+          <p className='p-2'>
+            genre: {genre}
+          </p>
+          <p className='p-2 w-lg text-balance '>
+            description: {description}
+          </p> 
+        </div> 
+      </div>
     </div>
   );
 }
