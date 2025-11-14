972baadeb617d6886b793093b37e243f447a9dc5
Lisa Vercruysse <lisa.vercruyss@student.hogent.be>
2025-11-14 14:49:44 +0100

    Finishing up advanced search

--- frontend-mybookshelf/src/components/TopBar.jsx
+++ frontend-mybookshelf/src/components/TopBar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 
 export default function TopBar(){
   return(
-    <div className="bg-emerald-900 text-emerald-50 p-2">
+    <div className="bg-emerald-900 text-emerald-50 p-2 mb-6">
       <Link to='/'>
         Overview
       </Link>
--- frontend-mybookshelf/src/pages/Discover.jsx
+++ frontend-mybookshelf/src/pages/Discover.jsx
@@ -6,21 +6,41 @@ import { getBooks } from '../api';
 import Book from '../components/Book';
 
 export default function Discover() {
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState([]);
   const [apiQuery, setApiQuery] = useState(null);
   const [page, setPage] = useState(0);
+  const [isAdvancedSearch, setAdvancedSearch] = useState(false);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [isbn, setIsbn] = useState('');
   const maxResults = 9;
 
   const{
     data: books = [],
     error,
     isLoading,
-  } = useSWR(apiQuery ? `?q=${apiQuery}&startIndex=${page * maxResults}&maxResults=${maxResults}&orderBy=newest
+  } = useSWR(apiQuery ? `?q=${apiQuery}
+    &startIndex=${page * maxResults}&maxResults=${maxResults}&orderBy=relevance
 ` : null, getBooks);
 
   const handleSearch = () => {
+    setPage(0);
+    if (isAdvancedSearch){
+      const filters = [];
+
+      if (title.trim()) filters.push(`intitle:${title.trim()}`);
+      if (author.trim()) filters.push(`inauthor:${author.trim()}`);
+      if (isbn.trim()) filters.push(`isbn:${isbn.trim()}`);
+
+      if (filters.length === 0){
+        alert('Please fill in at least 1 field');
+      }
+
+      const query = filters.join('+');
+      setApiQuery(encodeURIComponent(query));
+      return;
+    } 
     if (searchValue.trim()){
-      setPage(0);
       setApiQuery(encodeURIComponent(searchValue.trim().toLocaleLowerCase()));
     }
   };
@@ -35,23 +55,66 @@ export default function Discover() {
     }   
   };
 
+  const handleAdvancedSearch = () => {
+    if (isAdvancedSearch) {
+      setAdvancedSearch(false);
+      return;
+    } setAdvancedSearch(true);
+  };
+
   return (
     <>
       <TopBar/>
       <div className="flex flex-col items-center gap-5">
        
-        <div className='flex gap-3 pt-8'>
+        <div className={`${isAdvancedSearch ? 'hidden' : 'block'} flex gap-3 sm:items-center flex-col`}>
+          <div>
+            <input
+              className='border rounded-xl p-2 focus:outline-0 sm:w-lg mt-6'
+              type='search'
+              id='search'
+              placeholder='Search for books'
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+            <button type='button' className='border rounded-xl p-2 bg-emerald-900 text-emerald-50 
+          hover:bg-emerald-950 hover:cursor-pointer'
+            onClick={handleSearch}>Search</button>
+          </div>
+        </div>
+        <div className={`${isAdvancedSearch ? 'block': 'hidden'} flex flex-col gap-6`}>
           <input
-            className='border rounded-xl p-2 focus:outline-0'
+            className='border rounded-xl p-2 focus:outline-0 sm:w-lg'
             type='search'
-            id='search'
-            placeholder='Search for books'
-            onChange={(e) => setSearchValue(e.target.value)}
+            id='titleSearch'
+            placeholder='title'
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            className='border rounded-xl p-2 focus:outline-0 sm:w-lg'
+            type='search'
+            id='authorSearch'
+            placeholder='author'
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+          <input
+            className='border rounded-xl p-2 focus:outline-0 sm:w-lg'
+            type='search'
+            id='isbnSearch'
+            placeholder='isbn'
+            onChange={(e) => setIsbn(e.target.value)}
           />
           <button type='button' className='border rounded-xl p-2 bg-emerald-900 text-emerald-50 
           hover:bg-emerald-950 hover:cursor-pointer'
-          onClick={handleSearch}>Search</button>
+          onClick={handleSearch}>
+            Search
+          </button>
         </div>
+          
+        <button className='italic text-gray-400 border rounded-lg p-1 hover:cursor-pointer hover:border-gray-500 
+          hover:underline col-span-4 w-40' onClick={handleAdvancedSearch}>
+          {isAdvancedSearch ? 'normal search' : 'advanced search'}
+        </button> 
+        
         <div className='flex flex-wrap justify-center gap-10 max-w-400 mx-auto'>
           <AsyncData loading={isLoading} error={error}>
             {books.map((book) => {
