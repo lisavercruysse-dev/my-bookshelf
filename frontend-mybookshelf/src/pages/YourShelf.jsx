016f16efadb3b556fd582fec35f4aca790664c8a
Lisa Vercruysse <lisavercruysse.dev@gmail.com>
2026-08-04 14:21:27 +0200

    add new shelf

--- frontend-mybookshelf/src/pages/YourShelf.jsx
+++ frontend-mybookshelf/src/pages/YourShelf.jsx
@@ -1,14 +1,36 @@
 import useSWR from 'swr';
-import { getData } from '../api';
+import { getData, save } from '../api';
 import Shelf from '../components/shelves/Shelf';
 import AsyncData from '../components/asyncData/AsyncData';
+import { useState } from 'react';
+import Modal from '../components/general/modal';
+import { useForm, FormProvider } from 'react-hook-form';
+import LabelInput from '../components/LabelInput';
+import useSWRMutation from 'swr/mutation';
 
 export default function YourShelf() {
+  const [modal, setModal] = useState(false);
+  const methods = useForm();
+  const { handleSubmit, formState:{isValid} } = methods;
+
   const {
     data: shelves,
     error,
     isLoading,
   } = useSWR('shelves', getData);
+
+  const {
+    trigger: saveShelf, error: saveError,
+  } = useSWRMutation('shelves', save);
+
+  const onSubmit = async (values) => {
+    if (!isValid) return;
+
+    await saveShelf(values, {
+      throwOnError: false,
+      onSuccess: () => setModal(false),
+    });
+  };
 
   return (
     <div className='flex flex-col gap-2 px-10 py-8 max-w-6xl mx-auto'>
@@ -23,6 +45,33 @@ export default function YourShelf() {
           ))}
         </div>
       </AsyncData>
+      <button className='primary self-center' onClick={() => setModal(true)}>
+        Create New Shelf
+      </button>
+      <Modal isOpen={modal} onClose={() => setModal(false)}>
+        <div className="flex w-full flex-col items-center justify-center">
+          <p className='font-display justify-self-center text-2xl pb-5'>Add New Shelf</p>
+          <AsyncData error={saveError}>
+            <FormProvider {...methods}>
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="flex w-full max-w-sm flex-col items-center gap-4"
+              >
+                <LabelInput
+                  label=""
+                  name="title"
+                  placeholder="title"
+                  type="text"
+                  validationRules={{ required: 'Title is required' }}
+                />
+                <button type="submit" className="primary">
+                  Create
+                </button>
+              </form>
+            </FormProvider>
+          </AsyncData>
+        </div>
+      </Modal>
     </div>
   );
 }
