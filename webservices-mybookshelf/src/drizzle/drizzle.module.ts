ed9c44296cde56e0d2ff8d2287dca176a2644b85
lisavercruysse-dev <lisavercruysse.dev@gmail.com>
2026-08-12 18:46:14 +0200

    backend CI/CD

--- webservices-mybookshelf/src/drizzle/drizzle.module.ts
+++ webservices-mybookshelf/src/drizzle/drizzle.module.ts
@@ -1,17 +1,32 @@
-import { Module, OnModuleDestroy } from '@nestjs/common';
+import { Logger, Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import {
   type DatabaseProvider,
   DrizzleAsyncProvider,
   drizzleProvider,
   InjectDrizzle,
 } from './drizzle.provider';
+import path from 'node:path';
+import { migrate } from 'drizzle-orm/mysql2/migrator';
 
 @Module({
   providers: [...drizzleProvider],
   exports: [DrizzleAsyncProvider],
 })
-export class DrizzleModule implements OnModuleDestroy {
+// 👇 1
+export class DrizzleModule implements OnModuleDestroy, OnModuleInit {
+  private readonly logger = new Logger(DrizzleModule.name); // 👈 2
+
   constructor(@InjectDrizzle() private readonly db: DatabaseProvider) {}
+
+  // 👇 1
+  async onModuleInit() {
+    this.logger.log('⏳ Running migrations...');
+    // 👇 3
+    await migrate(this.db, {
+      migrationsFolder: path.resolve('migrations'),
+    });
+    this.logger.log('✅ Migrations completed!');
+  }
 
   async onModuleDestroy() {
     await this.db.$client.end();
